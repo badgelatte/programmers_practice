bb7381bc95e11e447263c2ca9fd02c787ba59b17
badgelatte <saucy5939@gmail.com>
2026-03-07 04:20:44 +0900

    fix: 프로그래머스 표 편집 수정
    ["C", "C", "C", "C", "Z"]일 경우 "OOXOOOOO"로 나오는 사태 발생으로 인해 list에서 array로 수정

--- programmers_practice/src/fatigue.java
+++ programmers_practice/src/fatigue.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Arrays;
 import java.util.List;
 
 public class fatigue {
@@ -8,51 +8,78 @@ public class fatigue {
         String[] arr
 //                = new String[]{"D 2","C","U 3","C","D 4","C","U 2","Z","Z"};
                 = new String[]{"D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z", "U 1", "C"};
+//                  = new String[]{"C", "C", "C", "C", "Z"};
         String answer = f.solution(8,2, arr);
         System.out.println(answer);
     }
     public String solution(int n, int k, String[] cmd) {
-        List<Integer> list = new ArrayList<>();
+        List<Integer> removeNums = new ArrayList<>();
+        String[] strArr = new String[n];
+        Arrays.fill(strArr, "O");
         StringBuilder answer = new StringBuilder();
         int num = 0;
 
         for(String a : cmd) {
             if(a.equals("C")) {
-                list.add(k);
-                n--;
+                strArr[k] = "X";
+                removeNums.add(k++);
+                if (k >= n) {
+                    k -= 2;
+                }
             }
             else if(a.equals("Z")) {
-                list.remove(list.size()- 1);
-                n++;
+                num = removeNums.remove(removeNums.size()- 1);
+                strArr[num] = "O";
             }
             else if(a.contains("U")) {
                 num = Integer.parseInt(a.split(" ")[1]);
-                k -= num;
-                if(k > n) {
+                k -= num; // -> 이걸 for문 앞으로 두고 조건문을 바꿀까? k가 0이전이면 그만큼 n에서 뺀만큼 뒤에서 비교하면 되잖아
+                // 만일 k = 0인데 7이 X이고 U 2 / n = 8 -> 이럴때 못 잡아냄
+                for (int removeNum : removeNums) {
+                    if (k < 0) {
+                        if (removeNum < k + num || (k + n <= removeNum && removeNum < n)) {
+                            k--;
+                        }
+                    }
+                    else if (k + num < removeNum && removeNum <= k) {
+                        k--;
+                    }
+                }
+
+                if(k < 0) {
                     k += n;
+                }
+
+                if (strArr[k].equals("X")) {
+                    k--;
                 }
             }
             else if(a.contains("D")) {
                 num = Integer.parseInt(a.split(" ")[1]);
                 k += num;
-                if(k < 0) {
+                for (int removeNum : removeNums) {
+                    if (k >= n) {
+                        if (removeNum <= k - num ||(k - num < removeNum && removeNum < n)) {
+                            k++;
+                        }
+                    }
+                    else if (k + num >= removeNum && removeNum > k) {
+                        k++;
+                    }
+                }
+
+                if(k >= n) {
                     k -= n;
+                }
+
+                if (strArr[k].equals("X")) {
+                    k++;
                 }
             }
         }
 
-        list.sort(Comparator.naturalOrder());
-        num = 0;
-        n += list.size();
-
-        for (int i = 0; i < n; i++) {
-            if (!list.isEmpty() && i == list.get(num)) {
-                answer.append("X");
-                list.removeFirst();
-            }
-            else {
-                answer.append("O");
-            }
+        for (String str : strArr) {
+            answer.append(str);
         }
 
         return answer.toString();
